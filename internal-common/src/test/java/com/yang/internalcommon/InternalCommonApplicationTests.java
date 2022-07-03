14f6603c4e6b780eb09fca5853aa00057969c26c
chunyang.li <ylovem@131499>
2022-07-03 11:17:53 +0800

    删除无用代码

--- internal-common/src/test/java/com/yang/internalcommon/InternalCommonApplicationTests.java
+++ internal-common/src/test/java/com/yang/internalcommon/InternalCommonApplicationTests.java
@@ -1,12 +1,7 @@
 package com.yang.internalcommon;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InternalCommonApplicationTests {
-
-    @Test
     void contextLoads() {
     }
 
