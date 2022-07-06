91770c1c2c1df5420559e47cba228165198c7fdb
chunyang.li <ylovem@131499>
2022-07-06 20:47:07 +0800

    add ribbon 灰度test

--- api-passenger/src/main/java/com/yang/apipassenger/controller/PassengerController.java
+++ api-passenger/src/main/java/com/yang/apipassenger/controller/PassengerController.java
@@ -1,15 +1,26 @@
 package com.yang.apipassenger.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/passenger")
 public class PassengerController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @PostMapping("/getPassengerInfo")
     public String getPassengerInfo(){
         return "my name is lcy";
+    }
+
+    @GetMapping("/call")
+    public String testCall(){
+        return restTemplate.getForObject("http://service-sms/test/sms-test",String.class);
     }
 }
